bb06da35d2afb2e62414abf88d11b1a31fdb96a1
Novikov Roman <service.for.roman@gmail.com>
2024-11-01 15:41:01 +0300

    fix monetary_obligations

--- parser/monetary_obligations.go
+++ parser/monetary_obligations.go
@@ -8,34 +8,54 @@ import (
 )
 
 type MonetaryObligation struct {
-	CreditorName   string
-	CreditorRegion string
-	Content        string
-	Basis          string
-	TotalSum       float64
-	DebtSum        float64
-	PenaltySum     float64
+	CreditorName     string
+	CreditorRegion   string
+	CreditorLocation string
+	Content          string
+	Basis            string
+	TotalSum         float64
+	DebtSum          float64
+	PenaltySum       float64
 }
 
-func FindMonetaryObligations(content string) (m []MonetaryObligation) {
-	m = make([]MonetaryObligation, 0)
+type MonetaryObligations struct {
+	Entrepreneurship    []MonetaryObligation
+	NonEntrepreneurship []MonetaryObligation
+}
+
+func FindMonetaryObligations(content string) (m MonetaryObligations) {
+	m = MonetaryObligations{
+		Entrepreneurship:    []MonetaryObligation{},
+		NonEntrepreneurship: []MonetaryObligation{},
+	}
 	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(content))
-	query := "MessageInfo StartOfExtrajudicialBankruptcy CreditorsNonFromEntrepreneurship MonetaryObligations MonetaryObligation"
-	doc.Find(query).Each(func(i int, s *goquery.Selection) {
+	s := doc.Find("MessageInfo StartOfExtrajudicialBankruptcy").First()
+	if s == nil || s.Length() == 0 {
+		return
+	}
+
+	m.Entrepreneurship = findMonetaryObligations(s, "CreditorsFromEntrepreneurship")
+	m.NonEntrepreneurship = findMonetaryObligations(s, "CreditorsNonFromEntrepreneurship")
+	return
+}
+
+func findMonetaryObligations(s *goquery.Selection, category string) (m []MonetaryObligation) {
+	m = []MonetaryObligation{}
+	s.Find(category + " MonetaryObligations MonetaryObligation").Each(func(i int, s *goquery.Selection) {
 		total, _ := strconv.ParseFloat(removeSpace(s.Find("TotalSum").Text()), 64)
 		debtSum, _ := strconv.ParseFloat(removeSpace(s.Find("DebtSum").Text()), 64)
 		penaltySum, _ := strconv.ParseFloat(removeSpace(s.Find("PenaltySum").Text()), 64)
 
 		m = append(m, MonetaryObligation{
-			CreditorName:   removeNewLine(s.Find("CreditorName").Text()),
-			CreditorRegion: removeNewLine(s.Find("CreditorRegion").Text()),
-			Content:        removeNewLine(s.Find("Content").Text()),
-			Basis:          removeNewLine(s.Find("Basis").Text()),
-			TotalSum:       total,
-			DebtSum:        debtSum,
-			PenaltySum:     penaltySum,
+			CreditorName:     removeNewLine(s.Find("CreditorName").Text()),
+			CreditorRegion:   removeNewLine(s.Find("CreditorRegion").Text()),
+			CreditorLocation: removeNewLine(s.Find("CreditorLocation").Text()),
+			Content:          removeNewLine(s.Find("Content").Text()),
+			Basis:            removeNewLine(s.Find("Basis").Text()),
+			TotalSum:         total,
+			DebtSum:          debtSum,
+			PenaltySum:       penaltySum,
 		})
 	})
-
 	return
 }
--- parser/monetary_obligations_test.go
+++ parser/monetary_obligations_test.go
@@ -10,63 +10,69 @@ func TestFindMonetaryObligations(t *testing.T) {
 	tests := []struct {
 		name     string
 		filepath string
-		want     []MonetaryObligation
+		want     MonetaryObligations
 	}{
 		{
 			name:     "arb",
 			filepath: "../testdata/monetaryObligations.xml",
-			want: []MonetaryObligation{
-				{
-					CreditorName:   "Общество с ограниченной ответственностью \"Ренессанс Кредит\"",
-					CreditorRegion: "г. Москва",
-					Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
-					Basis:          "Судебный приказ от 09.12.2022 №СП2-4624/2022-12 Судебный участок №12 г. Петрозаводска",
-					TotalSum:       63449.69,
-					DebtSum:        63449.69,
-					PenaltySum:     0,
-				},
-				{
-					CreditorName:   "Акционерное общество \"Банк Русский Стандарт\"",
-					CreditorRegion: "г. Москва",
-					Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
-					Basis:          "Судебный приказ от 15.01.2022 №СП2-4583/2021-12 Судебный участок №12 г. Петрозаводска",
-					TotalSum:       55020.91,
-					DebtSum:        55020.91,
-					PenaltySum:     0,
-				},
-				{
-					CreditorName:   "Публичное акционерное общество КБ \"Восточный\"",
-					CreditorRegion: "г. Москва",
-					Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
-					Basis:          "Судебный приказ от 22.06.2021 №СП2-1775/2021-12 Судебный участок №12 г. Петрозаводска",
-					TotalSum:       61694.17,
-					DebtSum:        61694.17,
-					PenaltySum:     0,
-				},
-				{
-					CreditorName:   "Публичное акционерное общество \"Совкомбанк\"",
-					CreditorRegion: "Костромская область",
-					Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
-					Basis:          "Судебный приказ от 28.05.2021 №СП2-1393/2021-12 Судебный участок №12 г. Петрозаводска",
-					TotalSum:       102928.34,
-					DebtSum:        102928.34,
-					PenaltySum:     0,
-				},
-				{
-					CreditorName:   "Публичное акционерное общество \"Совкомбанк\"",
-					CreditorRegion: "Костромская область",
-					Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
-					Basis:          "Судебный приказ от 25.05.2021 №СП2-1319/2021-12 Судебный участок №12 г. Петрозаводска",
-					TotalSum:       10272.32,
-					DebtSum:        10272.32,
-					PenaltySum:     0,
+			want: MonetaryObligations{
+				Entrepreneurship: []MonetaryObligation{},
+				NonEntrepreneurship: []MonetaryObligation{
+					{
+						CreditorName:   "Общество с ограниченной ответственностью \"Ренессанс Кредит\"",
+						CreditorRegion: "г. Москва",
+						Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
+						Basis:          "Судебный приказ от 09.12.2022 №СП2-4624/2022-12 Судебный участок №12 г. Петрозаводска",
+						TotalSum:       63449.69,
+						DebtSum:        63449.69,
+						PenaltySum:     0,
+					},
+					{
+						CreditorName:   "Акционерное общество \"Банк Русский Стандарт\"",
+						CreditorRegion: "г. Москва",
+						Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
+						Basis:          "Судебный приказ от 15.01.2022 №СП2-4583/2021-12 Судебный участок №12 г. Петрозаводска",
+						TotalSum:       55020.91,
+						DebtSum:        55020.91,
+						PenaltySum:     0,
+					},
+					{
+						CreditorName:   "Публичное акционерное общество КБ \"Восточный\"",
+						CreditorRegion: "г. Москва",
+						Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
+						Basis:          "Судебный приказ от 22.06.2021 №СП2-1775/2021-12 Судебный участок №12 г. Петрозаводска",
+						TotalSum:       61694.17,
+						DebtSum:        61694.17,
+						PenaltySum:     0,
+					},
+					{
+						CreditorName:   "Публичное акционерное общество \"Совкомбанк\"",
+						CreditorRegion: "Костромская область",
+						Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
+						Basis:          "Судебный приказ от 28.05.2021 №СП2-1393/2021-12 Судебный участок №12 г. Петрозаводска",
+						TotalSum:       102928.34,
+						DebtSum:        102928.34,
+						PenaltySum:     0,
+					},
+					{
+						CreditorName:   "Публичное акционерное общество \"Совкомбанк\"",
+						CreditorRegion: "Костромская область",
+						Content:        "Задолженность по кредитным платежам (кроме ипотеки)",
+						Basis:          "Судебный приказ от 25.05.2021 №СП2-1319/2021-12 Судебный участок №12 г. Петрозаводска",
+						TotalSum:       10272.32,
+						DebtSum:        10272.32,
+						PenaltySum:     0,
+					},
 				},
 			},
 		},
 		{
 			name:     "Not Found",
 			filepath: "../testdata/arb.xml",
-			want:     []MonetaryObligation{},
+			want: MonetaryObligations{
+				NonEntrepreneurship: []MonetaryObligation{},
+				Entrepreneurship:    []MonetaryObligation{},
+			},
 		},
 	}
 
